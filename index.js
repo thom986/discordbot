3c97e6765fef097ec593444b3dfc8aa8a2505db8
thom986 <66462203+thom986@users.noreply.github.com>
2021-02-03 09:16:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ client.on('ready', () => {
 
 });
 
-client.on("ready", async () => {
- 
-    console.log(`${client.user.username} is online.`);
- 
-    client.user.setActivity("Testing", { type: "PLAYING" });
- 
-});
-
-
-// THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);
+
+
